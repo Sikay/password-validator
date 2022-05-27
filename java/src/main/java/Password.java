27854a3467fb8292c09415fa6c87fd6bec1c74f8
FuenSansano <i.fuensanosano@gmail.com>
2022-05-27 11:10:46 +0200

    making fail should_have_8_characters

---
+++ java/src/main/java/Password.java
@@ -0,0 +1,8 @@
+public class Password {
+
+
+    public boolean validate(String password) {
+
+        return true;
+    }
+}
--- java/src/test/java/PasswordValidatorTest.java
+++ java/src/test/java/PasswordValidatorTest.java
@@ -1,12 +1,23 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class PasswordValidatorTest {
 
-	@Test
-	public void renameMe() {
-		assertTrue(true);
-	}
+    @Test
+    public void should_have_8_characters() {
+        Password password = new Password();
+        assertFalse(password.validate("hola"));
+    }
 
+    /*
+    @Test
+    public void should_have_a_capital_letter() {
+        Password password = new Password();
+        assertFalse(password.validate("hola"));
+    }
+
+     */
 }
+
