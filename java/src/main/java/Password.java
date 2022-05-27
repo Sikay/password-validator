5be513fb5d0e04dd95e7ebad7e0df02c6dab52b8
FuenSansano <i.fuensanosano@gmail.com>
2022-05-27 12:33:02 +0200

    create another branch

--- java/src/main/java/Password.java
+++ java/src/main/java/Password.java
@@ -1,26 +1,9 @@
 public class Password {
     private final int MIN_LENGTH = 8;
-    String numberRegex = "[0-9]+";
+    private final String REGEX = "[0-9a-zA-Z]+";
+    ;
 
     public boolean validate(String password) {
-        return minuminLengthPassword(password) && haveAnyCapitalLetter(password)
-                && haveAnyLowerLetter(password) && haveAnyNumber(password);
-    }
-
-
-    private boolean haveAnyNumber(String password) {
-        return !password.matches(numberRegex);
-    }
-
-    private boolean haveAnyLowerLetter(String password) {
-        return password.equals(password.toUpperCase());
-    }
-
-    private boolean haveAnyCapitalLetter(String password) {
-        return password.equals(password.toLowerCase());
-    }
-
-    private boolean minuminLengthPassword(String password) {
-        return (password.length() < MIN_LENGTH);
+        return !password.matches(REGEX);
     }
 }
