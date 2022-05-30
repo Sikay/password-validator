69dcc9cdf52d29dda5d4f2431c2c700344aaf536
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2022-05-30 19:12:57 +0200

    added should_not_validate_an_empty_password and made pass should_not_validate_a_negative_length

--- java/src/main/java/MinimumLengthRule.java
+++ java/src/main/java/MinimumLengthRule.java
@@ -1,7 +1,11 @@
 public class MinimumLengthRule {
     private int passwordLength;
+    private final static int MIN_PASSWORD_LENGTH = 0;
 
     public MinimumLengthRule(int passwordLength) {
+        if (passwordLength < MIN_PASSWORD_LENGTH) {
+            throw new IllegalArgumentException("Password length must be greater than 0");
+        }
         this.passwordLength = passwordLength;
     }
 
--- java/src/test/java/MinimumLengthRuleTest.java
+++ java/src/test/java/MinimumLengthRuleTest.java
@@ -1,25 +1,30 @@
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class MinimumLengthRuleTest {
+    private final static int PASSWORD_LENGTH = 6;
+
     @Test
     public void should_have_minimum_length() {
-        MinimumLengthRule minimumLength = new MinimumLengthRule(6);
+        MinimumLengthRule minimumLength = new MinimumLengthRule(PASSWORD_LENGTH);
         assertTrue(minimumLength.validate("holatu"));
     }
 
     @Test
     public void should_not_validate_a_too_short_password() {
-        MinimumLengthRule minimumLength = new MinimumLengthRule(6);
+        MinimumLengthRule minimumLength = new MinimumLengthRule(PASSWORD_LENGTH);
         assertFalse(minimumLength.validate("holat"));
     }
 
     @Test
+    public void should_not_validate_an_empty_password() {
+        assertThrows(IllegalArgumentException.class, () -> new MinimumLengthRule(0));
+    }
+
+    @Test
     public void should_not_validate_a_negative_length() {
-        MinimumLengthRule minimumLength = new MinimumLengthRule(-1);
-        assertFalse(minimumLength.validate("holat"));
+        assertThrows(IllegalArgumentException.class, () -> new MinimumLengthRule(-1));
     }
 }
