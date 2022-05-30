d93e6af24718a07138b7159c504ae1f3764fdbb0
Pedro Hernandez-Mora de Fuentes <pedrohdezmora@gmail.com>
2022-05-30 18:24:46 +0200

    red should_not_validate_a_too_short_password

--- java/src/test/java/MinimumLengthRuleTest.java
+++ java/src/test/java/MinimumLengthRuleTest.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
@@ -8,5 +9,11 @@ public class MinimumLengthRuleTest {
     public void should_have_minimum_length() {
         MinimumLengthRule minimumLength = new MinimumLengthRule(6);
         assertTrue(minimumLength.validate("holatu"));
+    }
+
+    @Test
+    public void should_not_validate_a_too_short_password() {
+        MinimumLengthRule minimumLength = new MinimumLengthRule(6);
+        assertFalse(minimumLength.validate("holat"));
     }
 }
