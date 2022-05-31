56101f0dc183a9f98ccc3b16703bfdde8d01f798
FuenSansano <i.fuensanosano@gmail.com>
2022-05-31 18:09:00 +0200

    creating given_a_password_without_number_should_not_validate_it

--- java/src/test/java/ContainsNumberRuleShould.java
+++ java/src/test/java/ContainsNumberRuleShould.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ContainsNumberRuleShould {
@@ -7,5 +8,11 @@ public class ContainsNumberRuleShould {
     public void given_number_validator_should_validate_a_text_with_number() {
         ContainsNumberRule containsNumberRule = new ContainsNumberRule();
         assertTrue(containsNumberRule.validate("texto88"));
+    }
+
+    @Test
+    public void given_a_password_without_number_should_not_validate_it() {
+        ContainsNumberRule containsNumberRule = new ContainsNumberRule();
+        assertFalse(containsNumberRule.validate("texto"));
     }
 }
