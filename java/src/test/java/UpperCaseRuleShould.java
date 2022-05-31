ede045cf20f149e679f2af037c9d58044db2ead5
FuenSansano <i.fuensanosano@gmail.com>
2022-05-31 17:31:40 +0200

    creatinggiven_upper_case_validator_should_not_validate_lower_case_text

--- java/src/test/java/UpperCaseRuleShould.java
+++ java/src/test/java/UpperCaseRuleShould.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class UpperCaseRuleShould {
@@ -7,5 +8,11 @@ public class UpperCaseRuleShould {
     public void given_upper_case_validator_should_validate_upper_case_text() {
         UpperCaseRule upperCaseRule = new UpperCaseRule();
         assertTrue(upperCaseRule.validate("unaMayuscula"));
+    }
+
+    @Test
+    public void given_upper_case_validator_should_not_validate_lower_case_text() {
+        UpperCaseRule upperCaseRule = new UpperCaseRule();
+        assertFalse(upperCaseRule.validate("todasminusculas"));
     }
 }
