db3a792bd1bf91ca611ff25324ca90329e00dc2d
FuenSansano <i.fuensanosano@gmail.com>
2022-05-31 18:26:15 +0200

    REFACTOR: password class

--- java/src/main/java/Password.java
+++ java/src/main/java/Password.java
@@ -1,19 +1,18 @@
-import java.util.regex.Pattern;
 
 public class Password {
     public static final int MINIMUN_LENGTH = 8;
-    String numberRegex = "[0-9]+";
 
     private MinimumLengthRule minimumLengthRule;
     private LowerCaseRule lowerCaseRule;
     private UpperCaseRule upperCaseRule;
     private ContainsNumberRule containsNumberRule;
 
-    public Password() {
-        this.minimumLengthRule = new MinimumLengthRule(MINIMUN_LENGTH);
-        this.lowerCaseRule = new LowerCaseRule();
-        this.upperCaseRule = new UpperCaseRule();
-        this.containsNumberRule = new ContainsNumberRule();
+    public  Password() {
+       this(
+               new MinimumLengthRule(MINIMUN_LENGTH),
+               new LowerCaseRule(),
+               new UpperCaseRule(),
+               new ContainsNumberRule());
     }
 
     public Password(MinimumLengthRule minimumLengthRule, LowerCaseRule lowerCaseRule, UpperCaseRule upperCaseRule, ContainsNumberRule containsNumberRule) {
@@ -33,11 +32,6 @@ public class Password {
 
     private boolean hasUnderscore(String password) {
         return password.contains("_");
-    }
-
-    private boolean hasNumber(String password) {
-        Pattern regexNumber = Pattern.compile(numberRegex);
-        return regexNumber.matcher(password).find();
     }
 
 }
