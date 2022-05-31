db612ef9f08b505d7e0d09de63524e932598f4fd
Pedro Hernandez-Mora de Fuentes <pedrohdezmora@gmail.com>
2022-05-31 17:41:45 +0200

    red given_a_lower_case_validator_should_validate_a_lower_case_and_upper_case_text

--- java/src/test/java/LowerCaseRuleShould.java
+++ java/src/test/java/LowerCaseRuleShould.java
@@ -11,6 +11,12 @@ public class LowerCaseRuleShould {
     }
 
     @Test
+    public void given_a_lower_case_validator_should_validate_a_lower_case_and_upper_case_text() {
+        LowerCaseRule lowerCaseRule = new LowerCaseRule();
+        assertTrue(lowerCaseRule.validate("mAYUSCULAS"));
+    }
+
+    @Test
     public void given_a_upper_case_validator_should_validate_a_lower_case_text() {
         LowerCaseRule lowerCaseRule = new LowerCaseRule();
         assertFalse(lowerCaseRule.validate("MAYUSCULAS"));
