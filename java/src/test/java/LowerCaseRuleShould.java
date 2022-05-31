9c477e98e345909c89da7434164b1ee4a574f43b
FuenSansano <i.fuensanosano@gmail.com>
2022-05-31 16:44:16 +0200

    creating given_a_upper_case_validator_should_validate_a_lower_case_text

--- java/src/test/java/LowerCaseRuleShould.java
+++ java/src/test/java/LowerCaseRuleShould.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class LowerCaseRuleShould {
@@ -7,5 +8,11 @@ public class LowerCaseRuleShould {
     public void given_a_lower_case_validator_should_validate_a_lower_case_text() {
         LowerCaseRule lowerCaseRule = new LowerCaseRule();
         assertTrue(lowerCaseRule.validate("minusculas"));
+    }
+
+    @Test
+    public void given_a_upper_case_validator_should_validate_a_lower_case_text() {
+        LowerCaseRule lowerCaseRule = new LowerCaseRule();
+        assertFalse(lowerCaseRule.validate("MAYUSCULAS"));
     }
 }
