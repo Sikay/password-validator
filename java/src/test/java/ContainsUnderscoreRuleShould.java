4f579f660160c4af7a8e3982aa01a6d6908bdeaf
FuenSansano <i.fuensanosano@gmail.com>
2022-06-01 16:54:54 +0200

    creating given_a_password_without_underscore_should_not_validate_the_password test

--- java/src/test/java/ContainsUnderscoreRuleShould.java
+++ java/src/test/java/ContainsUnderscoreRuleShould.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ContainsUnderscoreRuleShould {
@@ -7,5 +8,11 @@ public class ContainsUnderscoreRuleShould {
     public void given_underscore_validator_should_validate_a_text_with_underscore() {
         ContainsUnderscoreRule containsUnderscoreRule = new ContainsUnderscoreRule();
         assertTrue(containsUnderscoreRule.validate("textoCon_"));
+    }
+
+    @Test
+    public void given_a_password_without_underscore_should_not_validate_the_password() {
+        ContainsUnderscoreRule containsUnderscoreRule = new ContainsUnderscoreRule();
+        assertFalse(containsUnderscoreRule.validate("textoCon"));
     }
 }
